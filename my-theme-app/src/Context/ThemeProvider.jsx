8d516c782227da26ab1b548e20486f6a4eb2b07a
Viswak23 <Viswak.RameshBalakrishnan@metropolia.fi>
2024-10-07 01:09:27 +0530

    Add files via upload

---
+++ my-theme-app/src/Context/ThemeProvider.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import ThemeContext from './ThemeContext';
+
+export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light'); // Manage theme state
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark')); // Switch between themes
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}> 
+      {children}
+    </ThemeContext.Provider>
+  );
+}
